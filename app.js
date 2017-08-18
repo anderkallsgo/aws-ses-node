134ce00591418c7269ef0b4f2d32d262235eedc3
andersonkall <andersonkall@gmail.com>
2017-08-18 18:52:58 -0400

    subindo projeto

---
+++ app.js
@@ -0,0 +1,99 @@
+// Require objects.
+var express = require('express');
+var app     = express();
+var aws     = require('aws-sdk');
+
+// Edit this with YOUR email address.
+var email   = "diogo@dinneer.com";
+var to_email = "andersonkall@gmail.com";
+    
+// Load your AWS credentials and try to instantiate the object.
+aws.config.loadFromPath(__dirname + '/config.json');
+
+// Instantiate SES.
+var ses = new aws.SES();
+
+// Verify email addresses.
+app.get('/verify', function (req, res) {
+    var params = {
+        EmailAddress: email,
+    };
+
+    ses.verifyEmailAddress(params, function(err, data) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+
+// Listing the verified email addresses.
+app.get('/list', function (req, res) {
+    ses.listVerifiedEmailAddresses(function(err, data) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+
+// Deleting verified email addresses.
+app.get('/delete', function (req, res) {
+    var params = {
+        EmailAddress: email
+    };
+
+    ses.deleteVerifiedEmailAddress(params, function(err, data) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+
+// Sending RAW email including an attachment.
+app.get('/send', function (req, res) {
+    var ses_mail = "From: 'AWS Tutorial Series' <" + email + ">\n";
+    ses_mail = ses_mail + "To: " + to_email + "\n";
+    ses_mail = ses_mail + "Subject: AWS SES Attachment Example\n";
+    ses_mail = ses_mail + "MIME-Version: 1.0\n";
+    ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+    ses_mail = ses_mail + "--NextPart\n";
+    ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+    ses_mail = ses_mail + "Bom dia este é um e-mail automático de verificação de contas do www.dinneer.com."+
+        "Favor não respoder o mesmo, obrigado.\n\n";
+    ses_mail = ses_mail + "--NextPart\n";
+    ses_mail = ses_mail + "Content-Type: text/plain;\n";
+    ses_mail = ses_mail + "Content-Disposition: attachment; filename=\"attachment.txt\"\n\n";
+    ses_mail = ses_mail + "AWS Tutorial Series - Really cool file attachment!" + "\n\n";
+    ses_mail = ses_mail + "--NextPart--";
+    
+    var params = {
+        RawMessage: { Data: new Buffer(ses_mail) },
+        Destinations: [ to_email ],
+        Source: "'AWS Tutorial Series' <" + to_email + ">'"
+    };
+    
+    ses.sendRawEmail(params, function(err, data) {
+        if(err) {
+            res.send(err);
+        } 
+        else {
+            res.send(data);
+        }           
+    });
+});
+
+// Start server.
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('AWS SES example app listening at http://%s:%s', host, port);
+});
